d75e8b82897fc3b497b3b116f3b78dc7417e95aa
Betwulf <betwulf@hotmail.com>
2017-06-15 15:11:57 -0400

    imported pypng, works

--- make_mandel.py
+++ make_mandel.py
@@ -1,6 +1,16 @@
 from bmp import write_grayscale as bmp
+from bmp import dimensions as bmpdim
+
 import fractal
+import png
 
 mand = fractal.mandelbrot(448, 256)
 print(mand)
 bmp('mandel.bmp', mand)
+png.from_array(mand, 'L').save('mandel.png')
+
+w, h = bmpdim('mandel.bmp')
+print(bmpdim('mandel.bmp'))
+print("Width: ", w, " Height: ", h)
+
+
